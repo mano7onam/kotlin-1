icolizer/kotlin

JVM_IR: extract transient state (e.g. the visitor) from ClassCodegen
Since JvmBackendContext now holds onto ClassCodegens in order to
generate and cache inline method nodes in the same module, any state
stored in ClassCodegen directly affects JVM_IR peak memory consumption.
In particular, the visitor and the metadata serializer appear to
reference objects totaling about half the ClassCodegens' memory
consumption in my not very scientific tests.

This also has the benefit of making anything that would be inconsistent
under incremental compilation due to inline methods more visible, as
generateMethodNode is now statically guaranteed to not touch anything in
ClassCodegen.State.

Most of the diff is indentation; the only change that is somewhat of
substance is that fields are now generated only after all methods are
done to avoid adding even more hacks to generateAssertFieldIfNeeded.

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/codegen/AnnotationCodegen.kt
@@ -191,7 +191,7 @@ abstract class AnnotationCodegen(
         // See FileBasedKotlinClass.convertAnnotationVisitor
         generateSequence<IrDeclaration>(annotationClass) { it.parent as? IrDeclaration }.takeWhile { !it.isTopLevelDeclaration }.forEach {
             if (it is IrClass) {
-                innerClassConsumer.addInnerClass(it)
+                innerClassConsumer.innerClasses.add(it)
             }
         }
 

mano7onam/kotlin-1

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/codegen/AnnotationCodegen.kt
@@ -191,7 +191,7 @@ abstract class AnnotationCodegen(
         // See FileBasedKotlinClass.convertAnnotationVisitor
         generateSequence<IrDeclaration>(annotationClass) { it.parent as? IrDeclaration }.takeWhile { !it.isTopLevelDeclaration }.forEach {
             if (it is IrClass) {
-                classCodegen.addInnerClass(it)
+                classCodegen.innerClasses.add(it)
             }
         }
 